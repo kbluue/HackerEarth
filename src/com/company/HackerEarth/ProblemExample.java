83b15145b143669840fc4660cc9cecd26b846e9e
_kbluue_ <kbluue@yahoo.com>
2019-06-27 22:03:21 +0100

    Problem INterface created

---
+++ src/com/company/HackerEarth/ProblemExample.java
@@ -0,0 +1,19 @@
+package com.company.HackerEarth;
+
+import com.company.Problem;
+
+public class ProblemExample implements Problem {
+
+    private int a, b;
+
+    @Override
+    public String solution() {
+        return a + b + "";
+    }
+
+    @Override
+    public void initInput() {
+        a = 1;
+        b = 2;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.HackerEarth.ProblemExample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new ProblemExample().run();
+    }
+}

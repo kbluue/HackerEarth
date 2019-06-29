40c96d4e0a83594375cb8bfcb6744746ca1eff62
_kbluue_ <kbluue@yahoo.com>
2019-06-29 05:46:24 +0100

    Conclusion on Test algo

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,51 +1,18 @@
 package com.company;
 
-import java.lang.reflect.Method;
+public interface Test extends Problem {
 
-public class Test implements Problem {
+    void generateInput();
 
-    boolean autoInput;
-    Object problem;
+    void setManualInput();
 
-    Test(Object problem){
-        this.problem = problem;
-        Method[] methods = problem.getClass().getMethods();
-        for (Method m : methods){
-            System.out.println(m.toGenericString() + "\n");
-        }
-    }
-
-//    abstract void generateInput();
-//
-//    abstract void setManualInput();
-//
-//    abstract void additionalAction();
-
-    protected void setAutoInput(boolean autoInput) {
-        this.autoInput = autoInput;
-    }
+    void additionalAction();
 
     @Override
-    public String solution() {
-        return null;
-    }
-
-    @Override
-    public void initInput() {
-
-    }
-
-    @Override
-    public void run(){
-//        if (autoInput) generateInput();
-//        else setManualInput();
-//
-//        additionalAction();
+    default void run(){
+        generateInput();
+        setManualInput();
+        additionalAction();
         System.out.println(solution());
-    }
-
-    public boolean isProblem() {
-        Class<?> clazz = problem.getClass();
-        return Problem.class.isAssignableFrom(clazz);
     }
 }

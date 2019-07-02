df0c362d80e2337ce74a5bef927e193215fd504b
_kbluue_ <kbluue@yahoo.com>
2019-07-02 14:33:09 +0100

    adjust created and test

--- src/com/company/HackerEarth/JuneCircuit19/Rotation.java
+++ src/com/company/HackerEarth/JuneCircuit19/Rotation.java
@@ -1,4 +1,29 @@
 package com.company.HackerEarth.JuneCircuit19;
 
-public class Rotation {
+import com.company.Problem;
+
+import java.util.Scanner;
+
+public class Rotation implements Problem {
+
+    private int n;
+    private String s, t;
+
+    @Override
+    public String solution() {
+        return null;
+    }
+
+    @Override
+    public void initInput() {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        s = sc.next();
+        t = sc.next();
+    }
+
+    public String adjust(String in) {
+        return in.substring(1) + in.charAt(0);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,14 @@
 package com.company;
 
+import com.company.HackerEarth.JuneCircuit19.Rotation;
+
 public class Main {
 
     public static void main(String[] args) {
+        String test = "THink";
+        for (int i = 0; i < 5; i++) {
+            test = new Rotation().adjust(test);
+            System.out.println(test);
+        }
     }
 }

14720411fd1f86b286ed2e1a636e1569fe7fc3f5
_kbluue_ <kbluue@yahoo.com>
2019-07-02 21:05:23 +0100

    Folder HAck

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
-import com.company.HackerEarth.JuneCircuit19.Tests.RotationTest;
-
 public class Main {
 
     public static void main(String[] args) {
-        new RotationTest().run();
     }
 }

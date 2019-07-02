f6da6a215d09a629bd14bdbd8b8343aa68355307
_kbluue_ <kbluue@yahoo.com>
2019-07-02 14:20:12 +0100

    Master Clean State: No more merge her until the branch is confirmed complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,8 @@
 package com.company;
 
-import com.company.HackerEarth.JuneCircuit19.Tests.FunctionValueTest;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 11; i++) {
-            System.out.println(new FunctionValueTest().f(i));
-        }
     }
 }

d7790ab59f8f620d5f542cbd39f19500299721d7
_kbluue_ <kbluue@yahoo.com>
2019-07-02 21:33:04 +0100

    Initial commit: problem and test files created

---
+++ src/com/company/HackerEarth/JuneCircuit19/DetermineTheWinner.java
@@ -0,0 +1,16 @@
+package com.company.HackerEarth.JuneCircuit19;
+
+import com.company.Problem;
+
+public class DetermineTheWinner implements Problem {
+
+    @Override
+    public String solution() {
+        return null;
+    }
+
+    @Override
+    public void initInput() {
+
+    }
+}
---
+++ src/com/company/HackerEarth/JuneCircuit19/Tests/DTW_Test.java
@@ -0,0 +1,21 @@
+package com.company.HackerEarth.JuneCircuit19.Tests;
+
+import com.company.HackerEarth.JuneCircuit19.DetermineTheWinner;
+import com.company.Test;
+
+public class DTW_Test extends DetermineTheWinner implements Test {
+    @Override
+    public void generateInput() {
+
+    }
+
+    @Override
+    public void setManualInput() {
+
+    }
+
+    @Override
+    public void additionalAction() {
+
+    }
+}
